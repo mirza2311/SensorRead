46615fe367e6792d74619bc0480adce6b31ae893
Mirza <mirza_mahmutovic@hotmail.com>
2015-03-20 21:48:28 +0100

    TEst new app

---
+++ app/src/main/java/com/example/cb/mm/nh/sensorread/DotNetFragment.java
@@ -0,0 +1,18 @@
+package com.example.cb.mm.nh.sensorread;
+
+/**
+ * Created by Mirza on 2015-03-20.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class DotNetFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        return inflater.inflate(R.layout.dotnet_layout, container,false);
+    }
+}
--- app/src/main/java/com/example/cb/mm/nh/sensorread/MainActivity.java
+++ app/src/main/java/com/example/cb/mm/nh/sensorread/MainActivity.java
@@ -1,39 +1,52 @@
 package com.example.cb.mm.nh.sensorread;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-
-public class MainActivity extends ActionBarActivity {
-
+        import android.app.ActionBar;
+        import android.app.ActionBar.Tab;
+        import android.app.FragmentTransaction;
+        import android.os.Bundle;
+        import android.support.v4.app.FragmentActivity;
+        import android.support.v4.view.ViewPager;
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener{
+    ActionBar actionbar;
+    ViewPager viewpager;
+    FragmentPageAdapter ft;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewpager = (ViewPager) findViewById(R.id.pager);
+        ft = new FragmentPageAdapter(getSupportFragmentManager());
+        actionbar = getActionBar();
+        viewpager.setAdapter(ft);
+        actionbar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionbar.addTab(actionbar.newTab().setText("Java").setTabListener(this));
+        actionbar.addTab(actionbar.newTab().setText("Php").setTabListener(this));
+        actionbar.addTab(actionbar.newTab().setText(".Net").setTabListener(this));
+        viewpager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int arg0) {
+                actionbar.setSelectedNavigationItem(arg0);
+            }
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+                // TODO Auto-generated method stub
+            }
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
-
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+        // TODO Auto-generated method stub
     }
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        viewpager.setCurrentItem(tab.getPosition());
+    }
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+        // TODO Auto-generated method stub
     }
 }
